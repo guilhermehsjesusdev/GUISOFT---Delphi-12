f9f245a382add04ee6da867a042175eadaed4639
Guilherme Henrique de Sousa Jesus <guilherme.devsec@icloud.com>
2025-11-23 21:43:58 -0300

    V1 - Conexão com banco de dados

--- GSOFT.dpr
+++ GSOFT.dpr
@@ -1,17 +1,60 @@
-program GSOFT;
+﻿program GSOFT;
 
 uses
+  System.SysUtils,
+  Winapi.Windows,
   Vcl.Forms,
-  Main in 'View\Main.pas' {Form1},
-  LoadGauge in 'Intefaces\LoadGauge.pas',
+  SplashScreen in 'View\SplashScreen.pas' {SplasScreen},
+  Main in 'View\Main.pas' {FrmMain},
+  ConfigReader in 'infra\ConfigReader.pas',
+  DBConnection in 'infra\DBConnection.pas',
   LoadBarProgress in 'Intefaces\LoadBarProgress.pas',
-  Connection in 'Intefaces\Connection.pas';
+  LoadGauge in 'Intefaces\LoadGauge.pas';
 
 {$R *.res}
+
+var
+  FrmSplash: TSplashScreen;
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+
+  FrmSplash := TSplashScreen.Create(nil);
+  FrmSplash.Show;
+  FrmSplash.Update;
+
+  //Tempo mínimo de exibição do splash
+  Sleep(1500);
+  Application.ProcessMessages;
+
+  try
+    try
+      // Testa a conexão
+      TDBConnection.GetConnection;
+
+    except
+      on E: Exception do
+      begin
+        MessageBox(
+          0,
+          PChar('Falha ao conectar ao banco de dados:' + sLineBreak + E.Message),
+          'Erro',
+          MB_ICONERROR or MB_OK
+        );
+
+        FrmSplash.Free;
+        //Exit;
+      end;
+    end;
+
+    // Conexão OK → cria o Main e define como MainForm automaticamente
+    Application.CreateForm(TMain, FrmMain);
+
+  finally
+    FrmSplash.Free;
+  end;
+
   Application.Run;
 end.
+
